ccdfaadec9ef88a80343276981e1de35659984f8
Marcel <hello@machinateur.dev>
2022-02-18 14:13:40 +0100

    Add markdown extra support using custom converter class

---
+++ src/Twig/Extra/DefaultMarkdown.php
@@ -0,0 +1,62 @@
+<?php
+/*
+ * MIT License
+ *
+ * Copyright (c) 2021-2022 machinateur
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+namespace App\Twig\Extra;
+
+use ParsedownExtra;
+use Twig\Extra\Markdown\DefaultMarkdown as DefaultMarkdownBase;
+use Twig\Extra\Markdown\ErusevMarkdown;
+use Twig\Extra\Markdown\MarkdownInterface;
+
+/**
+ * Class DefaultMarkdown
+ * @package App\Twig\Extra
+ */
+class DefaultMarkdown extends DefaultMarkdownBase
+{
+    protected ?MarkdownInterface $converter;
+
+    public function __construct()
+    {
+        if (class_exists(ParsedownExtra::class)) {
+            $this->converter = new ErusevMarkdown(
+                new ParsedownExtra()
+            );
+
+            return;
+        }
+
+        parent::__construct();
+    }
+
+    public function convert(string $body): string
+    {
+        if ($this->converter) {
+            return $this->converter->convert($body);
+        }
+
+        return parent::convert($body);
+    }
+}
